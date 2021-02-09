8eacc1fbf0f0c427dc8d35d7ba118d0f009b563d
vrathbone <vanessa.rathbone@gmail.com>
2021-02-09 12:57:52 -0800

    initial set up (attempt #2), imported data

---
+++ mushroom.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(shiny)
+
+ui <- fluidPage(theme = "ocean.css",
+                
+                navbarPage("THIS IS MY TITLE!",
+                           tabPanel("Thing 1",
+                                    sidebarLayout(
+                                      sidebarPanel("WIDGETS!",
+                                                   checkboxGroupInput(inputId = "pick_species",
+                                                                      label = "Choose species:",
+                                                                      choices = unique(starwars$species)
+                                                   )
+                                      ),
+                                      mainPanel("OUTPUT!",
+                                                plotOutput("sw_plot"))
+                                    )
+                           ),
+                           tabPanel("Thing 2"),
+                           tabPanel("Thing 3")
+                           
+                )
+                
+)
+
+server <- function(input, output) {
+  
+  sw_reactive <- reactive({
+    
+    starwars %>%
+      filter(species %in% input$pick_species)
+    
+  })
+  
+  output$sw_plot <- renderPlot(
+    ggplot(data = sw_reactive(), aes(x = mass, y = height)) +
+      geom_point(aes(color = species))
+  )
+  
+}
+
+shinyApp(ui = ui, server = server)
