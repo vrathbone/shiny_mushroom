190ba504d9905329c85db08fa9145cd107399505
vrathbone <vanessa.rathbone@gmail.com>
2021-02-09 15:34:15 -0800

    added icons and wideget details to tabs 2 and 3

--- mushroom.R
+++ mushroom.R
@@ -8,12 +8,16 @@ library(here)
 #Read in data
 #mushrooms <- read_csv(here("data", "mushrooms.csv"))
 
-
-
 ui <- fluidPage(
-                navbarPage("Mushroom Foraging: A Beginer's Guide",
-                           theme = bs_theme(version = 4, bootswatch = "minty"),
-                           tabPanel("Getting Started",
+                navbarPage(
+                  #App title
+                  title = "Mushroom Foraging: A Beginner's Guide", #or "MY-cology - A Mushroom Finder"
+                  theme = bs_theme(version = 4, bootswatch = "minty"),
+                           
+                           #Intro Tab
+                           tabPanel(
+                             title = "Getting Started", 
+                             icon = icon("search"),
                                     sidebarLayout(
                                       sidebarPanel(h3("Filtering options",class = "text-success"),
                                                    checkboxGroupInput(inputId = "pick_variety",
@@ -31,15 +35,33 @@ ui <- fluidPage(
                                       mainPanel(h1("OUTPUT!", class = "text-secondary"),
                                                 icon("list", "fa-6x"), 
                                                 actionButton(inputId = "download_mush", 
-                                                             label= "Download list")),
-                                    )
-                           ),
-                           tabPanel("Find Your Foraging Grounds"),
-                           tabPanel("About")
+                                                             label= "Download list")))),
+                                      
+                           #Map Tab
+                           tabPanel("Find Foraging Grounds", icon = icon("map"),
+                              sidebarLayout(
+                                sidebarPanel(h3("Select your state", class = "text-success"),
+                                                  selectInput(inputId = "pick_state", 
+                                                             label = "Select a state", 
+                                                             choices = c("idk")), 
+                                                  actionButton(inputId = "go", 
+                                                             label = "Forage!", 
+                                                             status = "primary")
+                              ),
+                             mainPanel(
+                               h1("Map will go here!", class = "text-success")))),
                            
-                )
-                
-)
+                          #About Tab
+                           tabPanel(
+                             title = "About", 
+                             icon = icon("info-circle"),
+                              mainPanel(h1("About This Project"),
+                                h3("Mycology interactive app. We'll be visualizing the different varieties of mushrooms found in the US by city or mushroom type. Ideally, this app will serve as a way to explore what types of mushrooms grow, whether they're poisonous or not, and what types of environments they thrive in. We'd like for this to be a place where people can also add to the existing UCI mushroom database."),
+                                h2(strong("Methods of the data collected:")),
+                                print("Data was collected xyz, over 8000 samples are included in this dataset and 23 varieties of mushrooms."),
+                                h2(strong("About us:")),
+                                print("We're two masters students at the Bren School of Environmental Science and Management.")
+                              ))))
 
 server <- function(input, output) {
   
